447d6f27e6cd21b8cfeb67a27a419aa810060acd
Alexandra <allypatrascu@gmail.com>
2025-04-27 18:21:01 +0300

    added login and token verification functionality

--- auth-backend/index.ts
+++ auth-backend/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express'
 import axios from 'axios'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 const port = 3001
-const ioUrl = process.env.IO_URL || "localhost:3000"
+const ioUrl = process.env.IO_URL || "http://localhost:3000"
 
 app.use(express.json());
 
@@ -20,8 +21,8 @@ app.post('/register', async (req: Request, res: Response) => {
         // Check if the username is already taken
         const existingUser = await axios.get<{ exists: boolean }>(ioUrl + "/user/" + username);
         if (existingUser.data.exists) {
-            res.status(400).json("Username taken")
-            return
+            res.status(400).json({message: "Username taken"});
+            return;
         }
 
         // Hash the password
@@ -38,3 +39,42 @@ app.post('/register', async (req: Request, res: Response) => {
 })
 
 // Login route
+app.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if the username is already taken
+        const userValid = await axios.post<{ valid: boolean }>(ioUrl + "/user/valid", { username, password });
+        if (!userValid.data.valid) {
+            res.status(400).json({mesage: "User does not exist"});
+            return;
+        }
+
+        // Create token
+        const token = jwt.sign(
+            { username },
+            process.env.JWT_SECRET || 'DEV',
+            { expiresIn: '12h' }
+        );
+
+        res.json({ token });
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({message: "Error"});
+    }
+})
+
+// Verify token route
+app.get('/verify/:token', async (req: Request<{ token: string }>, res: Response) => {
+    try {
+        const { token } = req.params;
+    
+        // Decode the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'DEV');
+        
+        res.json(decoded);
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({message: "Error"});
+    }
+})
